b266a8edf38fb32111ed0234c8dda9acab774f87
Hyper <98272700+Hyperizer69@users.noreply.github.com>
2022-02-08 20:15:01 +0530

    Update Android.mk

--- Android.mk
+++ Android.mk
@@ -18,16 +18,4 @@ LOCAL_PATH := $(call my-dir)
 
 ifeq ($(TARGET_DEVICE),RMX1851)
 
-
-include $(CLEAR_VARS)
-LOCAL_MODULE := libqdMetaData
-LOCAL_MODULE_OWNER := Realme
-LOCAL_SRC_FILES_64 := proprietary/system_ext/lib64/libqdMetaData.so
-LOCAL_SRC_FILES_32 := proprietary/system_ext/lib/libqdMetaData.so
-LOCAL_MULTILIB := both
-LOCAL_MODULE_TAGS := optional
-LOCAL_MODULE_CLASS := SHARED_LIBRARIES
-LOCAL_MODULE_SUFFIX := .so
-include $(BUILD_PREBUILT)
-
 endif
